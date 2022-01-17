8f00abe96079d5d6588152a7cc6479c011973789
Florian LATAPIE <florian.latapie@etu.unice.fr>
2022-01-17 11:36:53 +0100

    ca affiche l'heure

--- src/main/java/com/example/betterclock/HelloController.java
+++ src/main/java/com/example/betterclock/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.betterclock;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
-public class HelloController {
+import java.net.URL;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Calendar calendar;
+
+    public HelloController(){
+        calendar = Calendar.getInstance();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            LocalTime currentTime = LocalTime.now();
+            welcomeText.setText(currentTime.getHour() + ":" + currentTime.getMinute() + ":" + currentTime.getSecond());
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+
     }
 }
